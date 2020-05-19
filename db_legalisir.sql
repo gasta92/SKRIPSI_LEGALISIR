420a0b65099ad247698b30eed32e5c83cdb49ee2
FaniFatina <fatina.ffani@gmail.com>
2020-05-19 17:53:43 +0700

    commit

--- db_legalisir.sql
+++ db_legalisir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3311
--- Generation Time: Apr 27, 2020 at 02:39 AM
--- Server version: 5.6.25
--- PHP Version: 5.5.27
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2020 at 12:53 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_bioadm`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_bioadm` (
+CREATE TABLE `tbl_bioadm` (
   `bioadmId` int(11) NOT NULL,
   `NIK` varchar(16) NOT NULL,
   `NAMA_LGKP` varchar(50) NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `tbl_bioadm` (
   `waktuDibuat` datetime NOT NULL,
   `diubahOleh` int(11) NOT NULL,
   `waktuDiubah` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_bioadm`
@@ -70,7 +70,7 @@ INSERT INTO `tbl_bioadm` (`bioadmId`, `NIK`, `NAMA_LGKP`, `JENIS_KLMIN`, `TMPT_L
 -- Table structure for table `tbl_jenisdok`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_jenisdok` (
+CREATE TABLE `tbl_jenisdok` (
   `jenisdokId` int(11) NOT NULL,
   `kode` varchar(3) NOT NULL,
   `nama` varchar(50) NOT NULL,
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `tbl_jenisdok` (
   `waktuDibuat` datetime NOT NULL,
   `diubahOleh` int(11) NOT NULL,
   `waktuDiubah` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_jenisdok`
@@ -88,9 +88,11 @@ CREATE TABLE IF NOT EXISTS `tbl_jenisdok` (
 INSERT INTO `tbl_jenisdok` (`jenisdokId`, `kode`, `nama`, `terhapus`, `dibuatOleh`, `waktuDibuat`, `diubahOleh`, `waktuDiubah`) VALUES
 (1, 'KK', 'Kartu Keluarga', 0, 4, '2020-03-09 04:31:55', 0, '0000-00-00 00:00:00'),
 (2, 'KTP', 'Kartu Tanda Penduduk', 0, 4, '2020-03-09 04:32:10', 0, '0000-00-00 00:00:00'),
-(3, 'AB', 'Akta Bantul', 0, 4, '2020-03-09 04:32:32', 4, '2020-03-09 04:33:53'),
-(4, 'ALB', 'Akta Luar Bantul', 0, 4, '2020-03-09 04:32:52', 0, '0000-00-00 00:00:00'),
-(5, 'tc', 'tes', 1, 4, '2020-03-09 04:34:13', 4, '2020-03-09 04:34:18');
+(3, 'AKL', 'Akta Kelahiran', 0, 4, '2020-03-09 04:32:32', 4, '2020-03-09 04:33:53'),
+(4, 'AKM', 'Akta Kematian', 0, 4, '2020-03-09 04:32:52', 0, '0000-00-00 00:00:00'),
+(5, 'BIO', 'Biodata WNI', 0, 4, '2020-03-09 04:34:13', 4, '2020-03-09 04:34:18'),
+(6, 'SKT', 'Surat Keterangan Perekaman KTP El', 0, 4, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(7, 'KLL', 'Akta Kelahiran Luar Bantul', 0, 4, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -98,7 +100,7 @@ INSERT INTO `tbl_jenisdok` (`jenisdokId`, `kode`, `nama`, `terhapus`, `dibuatOle
 -- Table structure for table `tbl_pejabat`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_pejabat` (
+CREATE TABLE `tbl_pejabat` (
   `pejabatId` int(11) NOT NULL,
   `nip` varchar(25) NOT NULL,
   `nama` varchar(50) NOT NULL,
@@ -108,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `tbl_pejabat` (
   `waktuDibuat` datetime NOT NULL,
   `diubahOleh` int(11) NOT NULL,
   `waktuDiubah` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_pejabat`
@@ -116,10 +118,11 @@ CREATE TABLE IF NOT EXISTS `tbl_pejabat` (
 
 INSERT INTO `tbl_pejabat` (`pejabatId`, `nip`, `nama`, `jabatan`, `terhapus`, `dibuatOleh`, `waktuDibuat`, `diubahOleh`, `waktuDiubah`) VALUES
 (1, '12345', 'Gasta45', 'Direktur45', 1, 1, '2020-02-06 13:07:09', 1, '2020-02-07 02:54:22'),
-(2, '321', 'Giska', 'Sekertaris', 0, 1, '2020-02-07 01:54:24', 0, '0000-00-00 00:00:00'),
-(3, '890', 'Rio', 'Manager', 0, 1, '2020-02-07 02:54:55', 0, '0000-00-00 00:00:00'),
+(2, '197511291998032003', 'Emmy Nikmawati, SH', 'Kabid PIAK dan Pemanfaatan Data', 0, 1, '2020-02-07 01:54:24', 1, '2020-05-15 19:14:47'),
+(3, '196810021996031003', 'Darwatiningsih, S.SI.M.SI', 'Kabid Pelayanan Pencatatan Sipil', 0, 1, '2020-02-07 02:54:55', 1, '2020-05-15 19:18:22'),
 (4, '7654321', 'Tes Aja1', 'Sekertaris1', 1, 1, '2020-02-09 11:16:15', 1, '2020-02-09 11:16:35'),
-(5, '8891', 'ayu1', 'Sekertaris II8', 1, 6, '2020-03-02 13:50:12', 6, '2020-03-02 13:50:34');
+(5, '8891', 'ayu1', 'Sekertaris II8', 1, 6, '2020-03-02 13:50:12', 6, '2020-03-02 13:50:34'),
+(6, '196405281997031002', 'Drs. Agustinus Bagus D.', 'Kasi Pengolahan Data dan Penyajian Data', 0, 1, '2020-05-15 19:20:11', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -127,7 +130,7 @@ INSERT INTO `tbl_pejabat` (`pejabatId`, `nip`, `nama`, `jabatan`, `terhapus`, `d
 -- Table structure for table `tbl_regleg`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_regleg` (
+CREATE TABLE `tbl_regleg` (
   `reglegId` int(11) NOT NULL,
   `pejabatId` int(11) NOT NULL,
   `jenisdokId` int(11) NOT NULL,
@@ -139,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `tbl_regleg` (
   `waktuDibuat` datetime NOT NULL,
   `diubahOleh` int(11) NOT NULL,
   `waktuDiubah` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_regleg`
@@ -148,7 +151,8 @@ CREATE TABLE IF NOT EXISTS `tbl_regleg` (
 INSERT INTO `tbl_regleg` (`reglegId`, `pejabatId`, `jenisdokId`, `nik`, `no_reg`, `tanggal`, `terhapus`, `dibuatOleh`, `waktuDibuat`, `diubahOleh`, `waktuDiubah`) VALUES
 (1, 2, 4, '876', 1, '2020-04-01', 0, 4, '2020-04-27 01:47:20', 0, '0000-00-00 00:00:00'),
 (2, 3, 4, '876', 1, '2020-04-03', 0, 4, '2020-04-27 01:51:24', 0, '0000-00-00 00:00:00'),
-(3, 3, 4, '876', 2, '2020-04-27', 0, 4, '2020-04-27 01:52:38', 0, '0000-00-00 00:00:00');
+(3, 3, 4, '876', 2, '2020-04-27', 0, 4, '2020-04-27 01:52:38', 0, '0000-00-00 00:00:00'),
+(4, 3, 2, '99728129', 1, '2020-05-19', 0, 1, '2020-05-19 12:09:15', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -156,10 +160,10 @@ INSERT INTO `tbl_regleg` (`reglegId`, `pejabatId`, `jenisdokId`, `nik`, `no_reg`
 -- Table structure for table `tbl_status`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_status` (
-  `statusId` tinyint(4) NOT NULL COMMENT 'status id',
-  `status` varchar(50) NOT NULL COMMENT 'status text'
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_status` (
+  `statusId` tinyint(4) NOT NULL COMMENT 'role id',
+  `status` varchar(50) NOT NULL COMMENT 'role text'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_status`
@@ -176,7 +180,7 @@ INSERT INTO `tbl_status` (`statusId`, `status`) VALUES
 -- Table structure for table `tbl_users`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_users` (
+CREATE TABLE `tbl_users` (
   `userId` int(11) NOT NULL,
   `email` varchar(128) NOT NULL COMMENT 'login email',
   `password` varchar(128) NOT NULL COMMENT 'hashed login password',
@@ -188,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
   `createdDtm` datetime NOT NULL,
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDtm` datetime DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_users`
@@ -201,7 +205,8 @@ INSERT INTO `tbl_users` (`userId`, `email`, `password`, `name`, `mobile`, `statu
 (4, 'user@user.com', '$2y$10$SAvFim22ptA9gHVORtIaru1dn9rhgerJlJCPxRNA02MjQaJnkxawq', 'Fani', '0858659512', 3, 0, 1, '2020-02-05 05:13:47', 1, '2020-02-05 05:14:49'),
 (6, 'boby@yahoo.com', '$2y$10$Gl44n./CI/0xaHjOV7vryeESGwubojAZxJEPjMar7lhH8hJ6TzK/q', 'Boby', '5432123456', 3, 0, 1, '2020-03-02 13:45:20', NULL, NULL),
 (5, 'giska98@yahoo.com', '$2y$10$9yWhQoXPO90E0iu87RTQN.uJ2fmzDrcBrpV3eZwXLsjAAKdcenYBu', 'Giska', '0857257710', 2, 0, 1, '2020-02-07 01:55:45', NULL, NULL),
-(7, 'andri1@gmail.com', '$2y$10$nZzfGBrBjA3tZ1PI0ZVqPOJbzVMqBEJ7H1i1I5t5p5yJZ5kbNh4.2', 'Andrikusuma1', '0858659581', 3, 0, 1, '2020-04-21 06:03:24', 1, '2020-04-21 06:03:52');
+(7, 'andri1@gmail.com', '$2y$10$nZzfGBrBjA3tZ1PI0ZVqPOJbzVMqBEJ7H1i1I5t5p5yJZ5kbNh4.2', 'Andrikusuma1', '0858659581', 3, 0, 1, '2020-04-21 06:03:24', 1, '2020-04-21 06:03:52'),
+(909, 'a@gmail.com', '12345', 'admin', '000000000', 1, 0, 0, '0000-00-00 00:00:00', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -246,7 +251,7 @@ ALTER TABLE `tbl_status`
 --
 ALTER TABLE `tbl_users`
   ADD PRIMARY KEY (`userId`),
-  ADD KEY `statusId` (`statusId`);
+  ADD KEY `roleId` (`statusId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -256,32 +261,32 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_bioadm`
 --
 ALTER TABLE `tbl_bioadm`
-  MODIFY `bioadmId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `bioadmId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tbl_jenisdok`
 --
 ALTER TABLE `tbl_jenisdok`
-  MODIFY `jenisdokId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `jenisdokId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tbl_pejabat`
 --
 ALTER TABLE `tbl_pejabat`
-  MODIFY `pejabatId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `pejabatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tbl_regleg`
 --
 ALTER TABLE `tbl_regleg`
-  MODIFY `reglegId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `reglegId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tbl_status`
 --
 ALTER TABLE `tbl_status`
-  MODIFY `statusId` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT 'status id',AUTO_INCREMENT=4;
+  MODIFY `statusId` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT 'role id', AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=910;
 --
 -- Constraints for dumped tables
 --
